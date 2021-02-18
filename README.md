2cf4a14cc93bf3380c3c3c963a9ccaf4b6e8cd53
spencer kelly <spencermountain@gmail.com>
2021-02-18 11:20:41 -0500

    setup a sinle command with basic flags

--- README.md
+++ README.md
@@ -1,11 +1,33 @@
 opinionated svelte boilerplate commands
 
-* `npx spencer-svelte/new`
-* `npx spencer-svelte/build`
-* `npx spencer-svelte/watch`
-* `npx spencer-svelte/pug`
+### CLI
+create a quick svelte app
+* `npx spencer-svelte -n myApp`
+
+build it
+* `npx spencer-svelte -b myApp`
+
+dev-mode hot-reloading 
+* `npx spencer-svelte -w myApp`
+
+dev-mode hot-reloading with pug compiling
+* `npx spencer-svelte -p myApp`
 
 it uses live-reload, rollup, and chokidar in a sloppy way - putting things together with low-amounts of thinking.
+
+### Pug-svelte setup
+create a `./Part.pug` file:
+
+```html
+<script>
+  import Part from './build/Part.html'
+</script>
+
+<div>
+  <Part />
+</div>
+```
+
 
 ### See also
 * [amble](https://github.com/spencermountain/amble) - for server-side reloading
---
+++ index.js
@@ -0,0 +1,61 @@
+#!/usr/bin/env node
+const sh = require('shelljs')
+const path = require('path')
+const server = require('./_lib/server')
+const build = require('./_lib/build')
+const watch = require('./_lib/watch')
+
+let args = process.argv.slice(2)
+let cmd = 'watch'
+args = args.filter((a) => {
+  if (a === '-w') {
+    return false
+  }
+  if (a === '-b') {
+    cmd = 'build'
+    return false
+  }
+  if (a === '-n') {
+    cmd = 'new'
+    return false
+  }
+  if (a === '-p') {
+    cmd = 'pug'
+    return false
+  }
+  return true
+})
+
+if (cmd === 'build') {
+  // build command
+  let name = args.join(' ') || ''
+  let dir = path.join(process.cwd(), `./${name}`)
+  // ok, go
+  build(dir)
+} else if (cmd === 'new') {
+  // new command
+  let name = args.join(' ')
+  let d = new Date()
+  let date = d.toISOString().substr(0, 10)
+  let dir = `./${name || date}`
+  let dest = path.join(__dirname, './defaults/')
+  sh.cp('-R', dest, dir)
+} else if (cmd === 'pug') {
+  let name = args.join(' ') || '/*.pug'
+  let dir = args.join(' ') || ''
+  let abs = path.join(process.cwd(), `./${dir}`)
+  server(abs)
+  watch(abs, () => {
+    let from = path.join(process.cwd(), `./${name}`)
+    let to = path.join(process.cwd(), `./${name}/build/`)
+    console.log('compiling', from)
+    sh.exec(`pug ${from} -o ${to}`)
+    build(abs)
+  })
+} else {
+  // watch command
+  let name = args.join(' ') || ''
+  let abs = path.join(process.cwd(), `./${name}`)
+  server(abs)
+  watch(abs, () => build(abs))
+}
